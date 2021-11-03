260f525b745f9d7e1bbd6bef2d34a88433234576
yannie4163 <73107567+aianney@users.noreply.github.com>
2021-11-03 09:47:05 +0800

    checking media querie

--- src/pages/paymentpage/paymentpage.component.jsx
+++ src/pages/paymentpage/paymentpage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PaymentStepper from '../../components/payment-method/payment-stepper/payment-stepper.component'
-import PaymentContent from '../../components/payment-method/payment-content/payment-content-one.component'
+import PaymentContent from '../../components/payment-method/payment-content/payment-content.component'
 import './paymentpage.styles.css'
 
 const PaymentPage = () => {
