37f45fc75129d2f94fe3fcaa87ea3ab6ca1c00b5
Diane De Guzman <d.deguzman@servoitsolutions.ph>
2021-11-07 10:30:19 +0800

    updating payment content

--- src/components/payment-method/payment-content/payment-content.component.jsx
+++ src/components/payment-method/payment-content/payment-content.component.jsx
@@ -6,63 +6,65 @@ import Box from '@mui/material/Box'
 import './payment-content.styles.css'
 import PaymentPriceBreakDown from '../payment-price-breakdown/payment-price-breakdown.component'
 import CustomButton from '../../custom-button/custom-button.component'
+import { Typography } from '@material-ui/core'
 
 const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    backgroundColor: '#f9f9f9;',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  backgroundColor: '#f9f9f9;',
 }))
 
 export default function PaymentContent() {
-    return (
-        <div className="payment">
-            <h1>Payment Method</h1>
-            <p>
-                Choose from the following mode of payments to complete your reservation
-            </p>
-            {/* details */}
-            <Box sx={{ width: '100%' }}>
-                <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={6}>
-                        <Item className="item">
-                            <span className="item_span_one">Reservation ID:</span>
-                            <span>DMDG - 10261028</span>
-                        </Item>
-                        <Item className="item">
-                            <span className="item_span_one">Rooms Reserved:</span>
-                            <span>4</span>
-                        </Item>
-                        <Item className="item">
-                            <span className="item_span_one">Reserved Dates:</span>
-                            <span>Oct. 26, 2021 - Oct. 28, 2021</span>
-                        </Item>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Item className="item">
-                            <span className="item_span_one">Name:</span>
-                            <span>Diane M. De Guzman</span>
-                        </Item>
-                        <Item className="item">
-                            <span className="item_span_one">Email:</span>
-                            <span>yanney@gmail.com</span>
-                        </Item>
-                        <Item className="item">
-                            <span className="item_span_one">Mobile No.:</span>
-                            <span>09263880835</span>
-                        </Item>
-                        <Item className="item">
-                            <span className="item_span_one">Address:</span>
-                            <span>Porac IIB Fiesta Communities</span>
-                        </Item>
-                    </Grid>
-                </Grid>
-                <PaymentPriceBreakDown />
-                {/* <TermsAndCondition /> */}
-                {/* <PaymentContentOne /> */}
-                <CustomButton className="payment-button">Finish</CustomButton>
-            </Box>
-        </div>
-    )
+  return (
+    <div className="payment">
+      <Typography variant="pageTitle">Payments</Typography>
+      {/* details */}
+      <Box sx={{ width: '100%' }}>
+        <Typography variant="pageSubtitle">
+          Choose from the following mode of payments to complete your
+          reservation
+        </Typography>
+        <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <Item className="item">
+              <span className="item_span_one">Reservation ID:</span>
+              <span>DMDG - 10261028</span>
+            </Item>
+            <Item className="item">
+              <span className="item_span_one">Rooms Reserved:</span>
+              <span>4</span>
+            </Item>
+            <Item className="item">
+              <span className="item_span_one">Reserved Dates:</span>
+              <span>Oct. 26, 2021 - Oct. 28, 2021</span>
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item className="item">
+              <span className="item_span_one">Name:</span>
+              <span>Diane M. De Guzman</span>
+            </Item>
+            <Item className="item">
+              <span className="item_span_one">Email:</span>
+              <span>yanney@gmail.com</span>
+            </Item>
+            <Item className="item">
+              <span className="item_span_one">Mobile No.:</span>
+              <span>09263880835</span>
+            </Item>
+            <Item className="item">
+              <span className="item_span_one">Address:</span>
+              <span>Porac IIB Fiesta Communities</span>
+            </Item>
+          </Grid>
+        </Grid>
+        <PaymentPriceBreakDown />
+        {/* <TermsAndCondition /> */}
+        {/* <PaymentContentOne /> */}
+        <CustomButton className="payment-button">Finish</CustomButton>
+      </Box>
+    </div>
+  )
 }
